7021bc7319f52412d10a27407e7401497335e165
Huyhoang-OH-28 <huyhoang.tran6669@gmail.com>
2024-01-31 19:13:16 +0700

    base layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,11 +3,25 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello world!</title>
     @vite('resources/css/app.css')
 </head>
 <body>
-<h1 class="text-3xl font-bold underline text-green-500">
-    Hello world!
-</h1>
+    <div class="app h-screen w-full grid  grid-cols-6 text-base">
+        <div class="col-span-1 w-full h-full bg-[#262948]">
+            <div class="w-full">
+            </div>
+        </div>
+        <div class="col-span-5 w-full h-full bg-[#3c425e]">
+            <div class="grid grid-cols-2 gap-1 h-full">
+                <div class="col-span-1 w-full h-full bg-[#202441]">
+
+                </div>
+                <div class="col-span-1 w-full h-full bg-[#212540]">
+
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
